38a88942577c1e9598e4561c2e77eb7721423502
project2501 <kaan_bardak@hotmail.com>
2026-08-10 19:00:52 -0400

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public bool hasDuplicate(int[] nums) {
+        Array.Sort(nums);
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if ((i != nums.Length -1) && nums[i] == nums[i+1])
+            {
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+}
