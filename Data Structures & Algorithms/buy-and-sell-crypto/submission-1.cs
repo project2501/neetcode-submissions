319f29ed89ef2d6d4c0c9ec41a17a3c524b6dda0
project2501 <kaan_bardak@hotmail.com>
2026-08-11 18:45:35 -0400

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+
+        int max = 0;
+
+        for (int i = 0; i < prices.Length; i++)
+        {
+            for (int j = i; j < prices.Length; j++)
+            {
+                if (max < prices[j])
+                    max = Math.Max(prices[j]-prices[i], max);
+            }
+        }
+
+        return max;
+    }
+}
